c6dadc6ad52108410a8a92d50e3827540390cac6
aceaway12 <aceaway12@yahoo.com>
2018-07-16 19:28:11 -0400

    Initial commit

---
+++ src/Duck.java
@@ -0,0 +1,11 @@
+
+public abstract class Duck {
+	public String imgpath;
+	public int atkMod;
+	public int defMod;
+	public boolean hasActed;
+	public String name;
+	public String flavortext;
+	public abstract boolean onPlayed();
+	public abstract boolean useAbility();
+}
