dca6b27112ae564e0a968a9c1e0fb13fb8595fa0
Adrian <assassinr3d@gmail.com>
2018-07-19 13:49:15 -0400

    new code

--- src/duckccg/cards/DuckMillhouse.java
+++ src/duckccg/cards/DuckMillhouse.java
@@ -1,5 +1,25 @@
 package duckccg.cards;
 
-public class DuckMillhouse {
-
+public class DuckMillhouse extends Duck {
+	
+	@Override
+	public boolean activateAbility() {
+		return true;
+	}
+	@Override
+	public int attack(int targetPos) {
+		
+	}
+	@Override
+	public int defend(int attackerPos) {
+	}
+	@Override
+	public void modStats(int atkVal, int defVal) {
+	}
+	@Override
+	public boolean die(int attackerPos) {
+	}
+	@Override
+	public boolean onPlayed() {
+	}
 }
